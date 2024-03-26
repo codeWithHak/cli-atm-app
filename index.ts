7660da59c58d9f29f3532b532bc5f076a313d31f
Uzair Ahmed Khan / HO / Zensoft <uzair.khan@ho.gfg.com.pk>
2024-03-27 01:26:25 +0500

    initialized project

---
+++ index.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+
+let myBalance = 10000;
+let myPin = 123;
+
+let pinAnswer = await inquirer.prompt([
+  {
+    message: "What's your pin",
+    name: "pin",
+    type: "number",
+  },
+]);
+
+if (pinAnswer.pin === myPin) {
+  console.log("Your pin is correct!");
+
+  inquirer.prompt([
+    {
+      name: "operation",
+      message: "Please Select An Option",
+      type: "list",
+      choices: ["Withdraw", "Check Balance"],
+    },
+  ]);
+} else {
+  console.log("Your pin is incorrect");
+}
